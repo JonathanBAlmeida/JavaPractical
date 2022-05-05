d68bcb738b7373bad9a34c2be3768c6530fc94f6
Jonathan Almeida <86905221+JonathanBAlmeida@users.noreply.github.com>
2022-05-05 17:36:06 +0530

    Add files via upload

---
+++ tybollean.java
@@ -0,0 +1,8 @@
+public class tybollean {
+    public static void main(String[] args){
+       boolean myBool = true; 
+       if(myBool == true) 
+       System.out.println("I am using a Boolean data type:"+myBool);
+    }
+    
+}
---
+++ tyfloat.java
@@ -0,0 +1,12 @@
+public class tyfloat {
+    public static void main(String args[]) { 
+        float myFloat1,myFloat2,result; 
+        myFloat1=1000.666f; 
+        myFloat2=110.77f; 
+        result=myFloat1-myFloat2; 
+        System.out.println("Number1: "+myFloat1 +" Number2: "+myFloat2 +" Number1-Number2: "+result); 
+        
+}
+    
+}
+ 
